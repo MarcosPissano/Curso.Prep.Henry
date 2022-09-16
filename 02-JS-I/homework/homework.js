5cc3a6dc25244e3f7dc838bb7e5e3b7d3b97759b
Marcos Marcoss <marcos_blu10@hotmail.com>
2022-09-15 23:45:17 -0300

    second comit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,10 +1,10 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = holaa
+const nuevaString = hola
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 1234
 
 // Crea una variable booleana:
 const nuevoBool = null;
